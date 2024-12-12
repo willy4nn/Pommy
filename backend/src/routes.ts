6f1709619251a4094950151b55a345400595c1bd
Willyan <willy4nnp@gmail.com>
2024-12-12 10:11:34 -0300

    backend/feature: add logout
    - Added POST /logout route
    - Clears authentication cookie to end session

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,7 @@ import { createUserController } from "./useCases/User/CreateUser";
 import { updateUserController } from "./useCases/User/UpdateUser";
 import { deleteUserController } from "./useCases/User/DeleteUser";
 import { loginUserController } from "./useCases/User/LoginUser";
+import { logoutUserController } from "./useCases/User/LogoutUser";
 
 const router = Router(); // Create a new instance of the router
 
@@ -24,6 +25,11 @@ router.delete("/users/:id", (request, response, next) => {
 // POST to login
 router.post("/login", (request, response, next) => {
 	return loginUserController.handle(request, response, next);
+});
+
+// POST to logout
+router.post("/logout", (request, response, next) => {
+	return logoutUserController.handle(request, response, next);
 });
 
 export { router }; // Export the router for use in other modules
