a355995962f68cc464469ee754844827aef8a053
Willyan <willy4nnp@gmail.com>
2025-02-12 17:55:34 -0300

    backend/feature: Add update, findById methods and tests for PostgresUsersRepository
    - Added `update` and `findById` methods to `PostgresUsersRepository`.
    - Created interfaces for the new methods.
    - Implemented unit tests for both methods to ensure correct functionality.

--- backend/src/repositories/implementations/PostgresUsersRepository.ts
+++ backend/src/repositories/implementations/PostgresUsersRepository.ts
@@ -4,6 +4,42 @@ import { IUsersRepository } from "../IUsersRepository";
 import { CustomError, ErrorCatalog } from "../../errors/CustomError";
 
 export class PostgresUsersRepository implements IUsersRepository {
+	// Method to find user by id
+	async findById(id: string): Promise<User> {
+		const client = await pool.connect();
+		try {
+			const res = await client.query(
+				"SELECT * FROM users WHERE id = $1",
+				[id]
+			);
+			if (res.rows.length > 0) {
+				const userRow = res.rows[0];
+
+				// Pass props and id to the User constructor
+				const user = new User(
+					{
+						name: userRow.name,
+						email: userRow.email,
+						password: userRow.password,
+					},
+					userRow.id,
+					new Date(userRow.created_at),
+					new Date(userRow.updated_at)
+				);
+
+				return user;
+			}
+			return null;
+		} catch (error) {
+			throw new CustomError(
+				ErrorCatalog.ERROR.USER.REPOSITORY.USER_NOT_FOUND,
+				error.message
+			);
+		} finally {
+			client.release(); // Release the client
+		}
+	}
+
 	// Method to find user by email
 	async findByEmail(email: string): Promise<User> {
 		const client = await pool.connect();
@@ -32,7 +68,7 @@ export class PostgresUsersRepository implements IUsersRepository {
 			return null;
 		} catch (error) {
 			throw new CustomError(
-				ErrorCatalog.ERROR.USER.REPOSITORY.QUERY_FAILED,
+				ErrorCatalog.ERROR.USER.REPOSITORY.USER_NOT_FOUND,
 				error.message
 			);
 		} finally {
@@ -58,6 +94,24 @@ export class PostgresUsersRepository implements IUsersRepository {
 		} catch (error) {
 			throw new CustomError(
 				ErrorCatalog.ERROR.USER.REPOSITORY.USER_SAVE_FAILED,
+				error.message
+			);
+		} finally {
+			client.release(); // Release the client
+		}
+	}
+
+	// Method to update user
+	async update(user: User): Promise<void> {
+		const client = await pool.connect();
+		try {
+			await client.query(
+				"UPDATE users SET name = $1, email = $2, password = $3, updated_at = $4 WHERE id = $5",
+				[user.name, user.email, user.password, user.updatedAt, user.id]
+			);
+		} catch (error) {
+			throw new CustomError(
+				ErrorCatalog.ERROR.USER.REPOSITORY.USER_UPDATE_FAILED,
 				error.message
 			);
 		} finally {
