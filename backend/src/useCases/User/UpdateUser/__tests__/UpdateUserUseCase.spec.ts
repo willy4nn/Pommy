63d0a678659abce9bc3edea9e76471f064817a0b
Willyan <willy4nnp@gmail.com>
2025-03-01 17:57:18 -0300

    backend/chore: Refactor unit and integration tests for UpdateUserUseCase and UpdateUserController
    - Refactored unit test for UpdateUserUseCase.
    - Refactored unit and integration tests for UpdateUserController.

--- backend/src/useCases/User/UpdateUser/__tests__/UpdateUserUseCase.spec.ts
+++ backend/src/useCases/User/UpdateUser/__tests__/UpdateUserUseCase.spec.ts
@@ -2,16 +2,15 @@ import { UpdateUserUseCase } from "../UpdateUserUseCase";
 import { PostgresUsersRepository } from "../../../../repositories/implementations/PostgresUsersRepository";
 import bcrypt from "bcrypt";
 import { v4 as uuidv4 } from "uuid";
+import { CustomError, ErrorCatalog } from "../../../../errors/CustomError";
 
 // Mocking the repository
 jest.mock(
 	"../../../../repositories/implementations/PostgresUsersRepository.ts"
 );
-
-// Mock bcrypt.hash explicitly with a return value
 jest.mock("bcrypt", () => ({
 	...jest.requireActual("bcrypt"),
-	hash: jest.fn().mockResolvedValue("HashedPassword123@"), // Set the return value for hash
+	hash: jest.fn().mockResolvedValue("HashedPassword123@"),
 }));
 
 describe("UpdateUserUseCase (Unit Tests)", () => {
@@ -22,9 +21,8 @@ describe("UpdateUserUseCase (Unit Tests)", () => {
 		mockPostgresUsersRepository =
 			new PostgresUsersRepository() as jest.Mocked<PostgresUsersRepository>;
 
-		// Mocking the 'update' function of the repository
 		mockPostgresUsersRepository.update = jest.fn().mockResolvedValue({
-			id: uuidv4(), // Generate a valid UUID
+			id: uuidv4(),
 			name: "updated user",
 			email: "updateduser@user.com",
 			password: "HashedPassword123@",
@@ -32,9 +30,8 @@ describe("UpdateUserUseCase (Unit Tests)", () => {
 			updatedAt: new Date(),
 		});
 
-		// Mocking the 'findById' function to simulate an existing user
 		mockPostgresUsersRepository.findById = jest.fn().mockResolvedValue({
-			id: uuidv4(), // Generate a valid UUID
+			id: uuidv4(),
 			name: "user",
 			email: "user@user.com",
 			password: "OldPassword123@",
@@ -47,7 +44,7 @@ describe("UpdateUserUseCase (Unit Tests)", () => {
 
 	it("should update the user successfully", async () => {
 		const userUpdateData = {
-			id: uuidv4(), // Generate a valid UUID for testing
+			id: uuidv4(),
 			name: "updated user",
 			email: "updateduser@user.com",
 			password: "NewPassword123@",
@@ -64,27 +61,26 @@ describe("UpdateUserUseCase (Unit Tests)", () => {
 				id: userUpdateData.id,
 				name: userUpdateData.name,
 				email: userUpdateData.email,
-				password: "HashedPassword123@", // Ensure the hashed password is passed
+				password: "HashedPassword123@", // Check if password is hashed
 			})
 		);
 	});
 
 	it("should throw an error if the user doesn't exist", async () => {
 		// Mocking findById to return null (user not found)
-		mockPostgresUsersRepository.findById = jest
-			.fn()
-			.mockResolvedValue(null);
+		mockPostgresUsersRepository.findById.mockResolvedValue(null);
 
 		await expect(
 			updateUserUseCase.execute({
-				id: uuidv4(), // Generate a valid UUID for testing
+				id: uuidv4(),
 				name: "user",
 				email: "user@user.com",
 				password: "Password123@",
 			})
-		).rejects.toThrow("User does not exist"); // Adjusting to check the correct message
+		).rejects.toThrow(
+			new CustomError(ErrorCatalog.ERROR.USER.SERVICE.USER_NOT_FOUND)
+		);
 
-		// Verifying that findById was called with the correct ID
 		expect(mockPostgresUsersRepository.findById).toHaveBeenCalledWith(
 			expect.any(String)
 		);
@@ -92,7 +88,7 @@ describe("UpdateUserUseCase (Unit Tests)", () => {
 
 	it("should not update fields that are not provided", async () => {
 		const userUpdateData = {
-			id: uuidv4(), // Generate a valid UUID for testing
+			id: uuidv4(),
 			email: "updateduser@user.com",
 		};
 
@@ -111,7 +107,7 @@ describe("UpdateUserUseCase (Unit Tests)", () => {
 
 	it("should hash the new password if provided", async () => {
 		const userUpdateData = {
-			id: uuidv4(), // Generate a valid UUID for testing
+			id: uuidv4(),
 			password: "NewPassword123@",
 		};
 
@@ -127,16 +123,22 @@ describe("UpdateUserUseCase (Unit Tests)", () => {
 	it("should throw an error if the repository fails to update the user", async () => {
 		// Mocking a failure in the repository's update method
 		mockPostgresUsersRepository.update.mockRejectedValue(
-			new Error("Failed to update")
+			new CustomError(
+				ErrorCatalog.ERROR.USER.REPOSITORY.USER_UPDATE_FAILED
+			)
 		);
 
 		await expect(
 			updateUserUseCase.execute({
-				id: uuidv4(), // Generate a valid UUID for testing
+				id: uuidv4(),
 				name: "updated user",
 				email: "updateduser@user.com",
 				password: "NewPassword123@",
 			})
-		).rejects.toThrow("Failed to update");
+		).rejects.toThrow(
+			new CustomError(
+				ErrorCatalog.ERROR.USER.REPOSITORY.USER_UPDATE_FAILED
+			)
+		);
 	});
 });
